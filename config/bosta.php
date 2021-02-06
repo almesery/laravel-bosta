f343faaf2ac1c4fb2de2ac8810bdb56948406c8b
Abdelrahman Almesery <abdelrahman.pme18@suezuni.edu.eg>
2021-02-06 12:04:54 +0200

    WIP

--- config/bosta.php
+++ config/bosta.php
@@ -1,6 +1,12 @@
 <?php
 
 return [
-    'api_key' => env('API_KEY', null),
-    'bosta_mode' => env('BOSTA_MODE', 'stage')
+    'stage' => [
+        'bosta_api_key' => env('STAGE_BOSTA_API_KEY', null),
+        'base_url' => env('STAGE_BASE_URL', 'https://stg-app.bosta.co'),
+    ],
+    'production' => [
+        'bosta_api_key' => env('PRODUCTION_BOSTA_API_KEY', null),
+        'base_url' => env('PRODUCTION_BASE_URL', 'https://app.bosta.co'),
+    ],
 ];
