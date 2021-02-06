2fac13606c7372a219ca7c5cc04f4f23b27c2986
Abdelrahman Almesery <abdelrahman.pme18@suezuni.edu.eg>
2021-02-06 12:14:06 +0200

    WIP

--- src/Bosta.php
+++ src/Bosta.php
@@ -2,68 +2,32 @@
 
 namespace Almesery\Bosta;
 
-use Almesery\Bosta\Cities\CityClient;
-use Almesery\Bosta\Deliveries\DeliveryClient;
-use Almesery\Bosta\PickupLocations\PickupLocationClient;
-use Almesery\Bosta\PickupRequests\PickupClient;
-use Almesery\Bosta\Zones\ZoneClient;
-use Illuminate\Config\Repository;
-use Illuminate\Contracts\Foundation\Application;
 
-/**
- * @property PickupClient pickup
- * @property PickupLocationClient pickupLocation
- * @property Repository|Application|mixed API_KEY
- * @property CityClient city
- */
 class Bosta
 {
     /**
-     * @var ZoneClient
+     * @var string
      */
-    private ZoneClient $zone;
+    protected string $apiKey;
 
     /**
-     * Create Bosta Instance
-     *
-     * @param string $API_KEY
-     * @param string $BASE_URL
+     * @var Http
      */
-    public function __construct(string $API_KEY, string $BASE_URL = 'https://app.bosta.co')
-    {
-        $this->BASE_URL = $BASE_URL . '/api/v1/';
-        $this->API_KEY = config('bosta.api_key');
-        $this->pickup = new PickupClient($this);
-        $this->pickupLocation = new PickupLocationClient($this);
-        $this->delivery = new DeliveryClient($this);
-        $this->city = new CityClient($this);
-        $this->zone = new ZoneClient($this);
-    }
+    protected Http $guzzle;
 
     /**
-     * Send Api Request
-     *
-     * @param string $method
-     * @param string $path
-     * @param object $body
-     * @param string $headers
-     * @return void
+     * @var string
      */
-    public function send(string $method, string $path, object $body, string $headers)
+    protected string $email;
+
+    /**
+     * @var string
+     */
+    protected string $password;
+
+    public function setApiKey(string $apiKey, $guzzle = null)
     {
-        $url = $this->BASE_URL . $path;
-        $curl = curl_init($url);
-        if ($method == 'DELETE') {
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "DELETE");
-        }
-        if ($method === 'POST' || $method === 'PUT') {
-            curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
-            curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($body));
-        }
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json", 'authorization:' . $this->API_KEY, 'X-Requested-By: php-sdk',));
-        $response = curl_exec($curl);
-        curl_close($curl);
-        return json_decode($response);
+        $this->apiKey = $apiKey;
+        $this->guzzle =
     }
 }
