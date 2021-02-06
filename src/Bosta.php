9731261c58db7157c497e762b993591a42e8b6bc
Abdelrahman Almesery <abdelrahman.pme18@suezuni.edu.eg>
2021-02-06 13:52:41 +0200

    WIP

--- src/Bosta.php
+++ src/Bosta.php
@@ -3,7 +3,6 @@
 namespace Almesery\Bosta;
 
 use Almesery\Bosta\Actions\ManagePickUpLocations;
-use Exception;
 use GuzzleHttp\Client;
 
 class Bosta
@@ -23,41 +22,15 @@ class Bosta
     protected Client $guzzle;
 
     /**
-     * @var string
-     */
-    protected string $email;
-
-    /**
-     * @var string
-     */
-    protected string $password;
-
-
-    /**
      * Bosta constructor.
      * @param null $apiKey
      * @param Client|null $guzzle
-     * @param string $loginMethod
-     * @param string|null $password
-     * @param string|null $email
-     * @throws Exception
      */
-    public function __construct($apiKey = null, Client $guzzle = null, string $loginMethod = 'account-credentials', $password = null, $email = null)
+    public function __construct($apiKey = null, Client $guzzle = null)
     {
 
-        if ($loginMethod == "apiKey") {
-            if (!is_null($apiKey)) {
-                $this->LoginByApiKey($apiKey, $guzzle);
-            }
-        }
-
-        if ($loginMethod == "accountCredentials") {
-            if (!is_null($this->password) && !is_null($this->email)) {
-                $this->LoginByAccountCredentials($this->email, $this->password)->post('/users/login', [
-                    'email' => $this->email,
-                    'password' => $this->password,
-                ]);
-            }
+        if (!is_null($apiKey)) {
+            $this->setApiKey($apiKey, $guzzle);
         }
 
         if (!is_null($guzzle)) {
@@ -70,7 +43,7 @@ class Bosta
      * @param Client|null $guzzle
      * @return $this
      */
-    public function LoginByApiKey(string $apiKey, Client $guzzle = null): Bosta
+    public function setApiKey(string $apiKey, Client $guzzle = null): Bosta
     {
         $this->apiKey = $apiKey;
         $this->guzzle = $guzzle ?: new Client([
@@ -85,29 +58,4 @@ class Bosta
         ]);
         return $this;
     }
-
-
-    /**
-     * @param string $email
-     * @param string $password
-     * @param Client|null $guzzle
-     * @return $this
-     */
-    public function LoginByAccountCredentials(string $email, string $password, Client $guzzle = null): Bosta
-    {
-        $this->email = $email;
-        $this->password = $password;
-        $this->guzzle = $guzzle ?: new Client([
-            'base_uri' => config('bosta.production.base_url'),
-            "http_errors" => 'false',
-            'headers' => [
-                "Content-Type" => "application/json",
-                "Accept" => "application/json",
-                'authorization' => $this->apiKey,
-                'X-Requested-By' => 'php-sdk',
-            ],
-        ]);
-        return $this;
-    }
-
 }
